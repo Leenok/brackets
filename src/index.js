b6f6e924d328665669318e570e3ce3808f6ddb07
Leenok <34567431+Leenok@users.noreply.github.com>
2022-07-31 02:29:03 +0300

    init: add solu 1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  return true;
+  let otk = ['(', '[', '{'];
+	let znk =[',', "'", " "];
+	let komSkob = {
+	  ['(']:')',
+	  ['[']:']',
+	  ['{']:'}',
+	  ["'"]:"'",
+	}
+	let stek = [];
+	for(let i=0; i<str.length; i++){
+		if(otk.includes(str[i])){
+			stek.push(str[i]);
+		}else{
+      	if(znk.includes(str[i])){
+        	continue;
+      	}
+      let topE = stek[stek.length-1];
+      	if(komSkob[topE] == str[i] ){
+				stek.pop();
+      	}
+    	}
+    
+	}
+	return stek.length === 0;
 }
